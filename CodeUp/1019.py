c8e311a9755a2cadffc2d5c8e3f5dfb652b377cf
songyoungeun <young5481@gmail.com>
2020-08-06 21:22:42 +0900

    CodeUp

--- CodeUp/1019.py
+++ CodeUp/1019.py
@@ -1,2 +1,4 @@
-a, b, c = map(int,input().split('.'))
-print("%04d.%02d.%02d"%(a,b,c))
+# 1019 : [기초-입출력] 연월일 입력받아 그대로 출력하기
+
+a, b, c = map(int,input().split('.')) #2013.8.5
+print("%04d.%02d.%02d"%(a,b,c)) #2013.08.05
