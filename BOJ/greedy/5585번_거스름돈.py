45374ea46ba6b5b627bda19b459fb747144dfb5a
songyoungeun <young5481@gmail.com>
2020-08-19 17:34:40 +0900

    5585번_거스름돈.py

--- BOJ/greedy/5585번_거스름돈.py
+++ BOJ/greedy/5585번_거스름돈.py
@@ -1,23 +1,37 @@
+# import sys
+# result = 0
+# m = int(sys.stdin.readline())
+# m = 1000 - m
+
+# result += m // 500
+# m = m % 500
+
+# result += m // 100
+# m = m % 100
+
+# result += m // 50
+# m = m % 50
+
+# result += m // 10
+# m = m % 10
+
+# result += m // 5
+# m = m % 5
+
+# result += m // 1
+
+# print(result)
 import sys
-result = 0
-m = int(sys.stdin.readline())
-m = 1000 - m
 
-result += m // 500
-m = m % 500
+n = int(sys.stdin.readline())
+n = 1000 - n
+count = 0
 
-result += m // 100
-m = m % 100
+# 큰 단위의 화폐부터 차례대로 확인하기
+coin_types = [500, 100, 50, 10, 5, 1]
 
-result += m // 50
-m = m % 50
+for coin in coin_types:
+    count += n // coin # 해당 화폐로 거슬러 줄 수 있는 동전의 개수 세기
+    n %= coin
 
-result += m // 10
-m = m % 10
-
-result += m // 5
-m = m % 5
-
-result += m // 1
-
-print(result)
+print(count)
