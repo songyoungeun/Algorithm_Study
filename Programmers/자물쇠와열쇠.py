4c8120f834cee8a5f124a130ff5800d30e94c689
songyoungeun <young5481@gmail.com>
2020-09-08 00:49:16 +0900

    자물쇠와열쇠.py

---
+++ Programmers/자물쇠와열쇠.py
@@ -0,0 +1,52 @@
+key = [[0, 0, 0],
+        [1, 0, 0],
+        [0, 1, 1]]
+lock = [[1, 1, 1],
+        [1, 1, 0],
+        [1, 0, 1]]
+def rotate90(arr):
+    n = len(arr) #행
+    m = len(arr[0]) #열
+    result = [[0] * n for _ in range(m)]
+    for i in range(n):
+        for j in range(m):
+            result[j][n - i - 1] = arr[i][j]
+    return result
+def check(new_lock):
+    #자물쇠 중간 부분이 모두 1인지 확인
+    lock_length = len(new_lock) // 3
+    for i in range(lock_length, lock_length * 2):
+        for j in range(lock_length, lock_length * 2):
+            if new_lock[i][j] != 1:
+                return False
+    return True
+    
+def solution(key, lock):
+    n = len(lock)
+    m = len(key)
+    #기존 자물쇠 크기를 3배로 변환
+    new_lock = [[0] * (n * 3) for _ in range(n * 3)]
+    #새로운 자물쇠의 중앙 부분에 기존의 자물쇠 넣기
+    for i in range(n):
+        for j in range(n):
+            new_lock[i +n][j +n] = lock[i][j]
+    #4가지 방향에 대해서 확인
+    for rotation in range(4):
+        key = rotate90(key) #열쇠 회전
+        for x in range(n * 2):
+            for y in range(n * 2):
+                #자물쇠에 열쇠를 끼워 넣기
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x + i][y + j] += key[i][j]
+                #새로운 자물쇠에 열쇠가 정확히 들어맞는지 검사
+                if check(new_lock) == True:
+                    return True
+                #자물쇠에서 열쇠를 다시 빼기
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x + i][y + j] -= key[i][j]
+    return False
+
+
+print(solution(key,lock))
