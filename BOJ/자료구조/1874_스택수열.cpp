2e62f8d09fd5929e6120c3f3b4c8aa0991744d10
songyoungeun <young5481@gmail.com>
2021-01-31 18:32:42 +0900

    1874_스택수열[스택]

---
+++ BOJ/자료구조/1874_스택수열.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+	int n, num; 
+	int cnt = 1;
+	stack<int> s;
+	string result ="";
+	cin >> n;
+	
+	for(int i = 0; i < n; i++) {
+		cin >> num;
+		if(num >= cnt){
+			while(num +1 != cnt){
+				s.push(cnt++);
+				result += "+\n";
+			}
+			s.pop();
+			result += "-\n";
+		}
+		else{
+			if(s.top() == num){
+				s.pop();
+				result += "-\n";
+			}  
+			else{
+				result = "NO";
+				break;
+			}
+		}
+	}
+	cout << result;
+	return 0;
+
+	
+}
