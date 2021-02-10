607a992cb064be7a5e63dc8e94f1acf157abb990
songyoungeun <young5481@gmail.com>
2021-02-10 12:59:28 +0900

    15688_수정렬하기5[counting_sort]

---
+++ BOJ/탐색과정렬/15688_수정렬하기5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int freq[2000001];
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		int idx;
+		cin >> idx;	
+		freq[idx+1000000]++; //-1000000 ~ 1000000
+	} 
+	for(int i = 0; i <= 2000000; i++){
+		while(freq[i]--){
+			cout<< i - 1000000 << '\n';
+		}		
+	}
+
+}
