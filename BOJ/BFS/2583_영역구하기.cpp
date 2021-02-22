50354bbec7af9b30b3fc70f3c3e96245a5a3ebca
songyoungeun <young5481@gmail.com>
2021-02-23 00:41:21 +0900

    2583_영역구하기[bfs]

--- BOJ/BFS/2583_영역구하기.cpp
+++ BOJ/BFS/2583_영역구하기.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <queue>
 #include <algorithm>
-using namespace std;
+
 #define X first
 #define Y second
+using namespace std;
+
 int m, n, k;
 int x1,x2,y1,y2;
 int board[101][101];
 int vis[101][101];
+
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1}; // 상하좌우 네 방향을 의미
 vector <int>v;
+
 int main(){
 	ios::sync_with_stdio(0);
 	cin.tie(0);
@@ -24,39 +28,32 @@ int main(){
 			}		
 		}
 	} 
-	
-	for(int i = 0; i < m; i++){
-		for(int j = 0; j < n; j++){
-			cout << board[i][j];
-		}cout << '\n';
-	}
 	int num = 0; //영역의 수 
 
 	for(int i = 0; i < m; i++){
 		for(int j = 0; j < n; j++){
 			if(board[i][j] != 0 or vis[i][j]) continue; 
 			num++; 
-			cout << "num" << num <<'\n';
 			queue<pair<int,int> > q;
 			vis[i][j] = 1;
 			q.push({i,j});
 			int area = 0; //넓이  
 			while(!q.empty()){
-				area++;
+				area++;  
 				pair<int,int> cur = q.front(); q.pop();
-				for(int idx = 0; i < 4; i++){
+				for(int idx = 0; idx < 4; idx++){
 					int nx = cur.X + dx[idx];
 					int ny = cur.Y + dy[idx];
-					if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-					if(vis[nx][ny] || board[nx][ny] != 0) continue;
+          			if(nx < 0 || nx >= m || ny < 0 || ny >= n) continue; 
+          			if(vis[nx][ny] || board[nx][ny] != 0) continue;
 					vis[nx][ny] = 1;
 					q.push({nx,ny});
 				}
 			}
-			v.push_back(area);
-			
+			v.push_back(area);			
 		}
 	}
+	
 	cout << num <<'\n';
 	sort(v.begin(),v.end());
 	for(int i = 0; i < num; i++) cout << v[i] << ' ';
