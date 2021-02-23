328c65da94fb519963b46b1b2e1f377e0a23ec86
songyoungeun <young5481@gmail.com>
2021-02-23 14:47:16 +0900

    1463_1로만들기[dp]

--- BOJ/dp/1463_1로만들기.cpp
+++ BOJ/dp/1463_1로만들기.cpp
@@ -10,10 +10,10 @@ int main(){
 	cin >> n;
 	dp[1] = 0; //�ʱⰪ  
 	for(int i = 2; i <= n; i++){
-		d[i] = d[i-1] +1;
-		if(i % 2 == 0) dp[i] = min(d[i], d[i/2]+1);
-		if(i % 3 == 0) dp[i] = min(d[i], d[i/3]+1);
+		dp[i] = dp[i-1] +1;
+		if(i % 2 == 0) dp[i] = min(dp[i], dp[i/2]+1);
+		if(i % 3 == 0) dp[i] = min(dp[i], dp[i/3]+1);
 
 	}
-	cout << d[n];
+	cout << dp[n];
 }
