54d4ed01ed074659f647c5fadedc4ea6c522b22a
songyoungeun <young5481@gmail.com>
2021-02-23 23:44:18 +0900

    11726_2xn타일링[dp]

---
+++ BOJ/dp/11726_2xn타일링.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int d[1005];
+int mod = 10007;
+ 
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	d[1] = 1;
+	d[2] = 2;
+	for(int i = 3; i <= n; i++){
+		d[i] = (d[i-1] + d[i-2]) % mod; 
+	}
+	cout << d[n];
+}
