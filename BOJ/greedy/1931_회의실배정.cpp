96d49fbae0e94ab24d96deb720d7b4f7c2bd70d7
songyoungeun <young5481@gmail.com>
2021-02-26 23:40:41 +0900

    1931_회의실배정[그리디]

---
+++ BOJ/greedy/1931_회의실배정.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, i, j;
+pair<int,int> s[100005]; //{end time, start time}
+
+int main(void){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n;
+	for(int i = 0; i < n; i++)
+		cin >> s[i].second >> s[i].first;
+	sort(s, s + n); 
+	int ans = 0;
+	int t = 0;
+	for(int i = 0; i < n; i++){
+		if(t > s[i].second) continue;
+		ans++;
+		t = s[i].first;
+	}
+	cout << ans;
+} 
