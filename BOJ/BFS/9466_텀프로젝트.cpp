7da16d1c729db10307887049e19966c62bcf0a3f
songyoungeun <young5481@gmail.com>
2021-03-12 00:53:00 +0900

    9466_텀프로젝트[bfs]

--- BOJ/BFS/9466_텀프로젝트.cpp
+++ BOJ/BFS/9466_텀프로젝트.cpp
@@ -1,58 +1,55 @@
-#include<iostream>
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<math.h>
-#include<algorithm>
-
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <cstring>
 using namespace std;
 
-#define MAX 100001
+int t, n;
+int student[100005];
+bool vis[100005];
 
-int testcase, cnt;
-int arr[MAX];
-bool visit[MAX], check[MAX];
-
-void DFS(int index) {
-	int next_num = arr[index];
-	visit[index] = true;
-
-	if (!visit[next_num])
-		DFS(next_num);
-	else {
-		if (!check[next_num]) {
-			for (int i = next_num; i != index; i = arr[i]) cnt++;
-			cnt++;
+int bfs(int start, vector<int>& circuit){
+	queue<int> q;
+	q.push(start);
+	vis[start] = 1;
+	while(!q.empty()){
+		int cur = q.front();
+		q.pop();
+		circuit.push_back(cur);
+		int num = student[cur];
+		if(!vis[num]){
+			vis[num] = 1;
+			q.push(num);
 		}
+		else 
+			return num;
 	}
-	check[index] = true;
 }
 
-void Memset_Arr() {
-	memset(arr, 0, sizeof(arr));
-	memset(visit, false, sizeof(visit));
-	memset(check, false, sizeof(check));
-}
-
-void init() {
-	ios::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
-}
-
-int main() {
-	init();
-	cin >> testcase;
-	
-	while (testcase--) {
-		Memset_Arr();
-		int N; cin >> N;
-
-		for (int i = 1; i <= N; i++) cin >> arr[i];
-		cnt = 0;
-		for (int i = 1; i <= N; i++)
-			if (!visit[i])
-				DFS(i);
-		cout << N - cnt << endl;
+int main(void){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> t;
+	while(t--){
+		memset(vis, false, sizeof(vis));
+		cin >> n;
+		for(int i = 1; i <= n; i++){
+			cin >> student[i];
+		}
+		int result = 0;
+		for(int i = 1; i <= n; i++){
+			if(!vis[i]){
+				vector<int> circuit;
+				int idx = bfs(i,circuit);
+				int len = circuit.size();
+				for(int i = 0; i < len; i++){
+					if(idx == circuit[i]) break;
+					result ++;
+				}
+				
+			}
+		}cout << result << '\n';
+		
 	}
 	return 0;
 }
