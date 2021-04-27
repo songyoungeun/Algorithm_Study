bb0bd7f6afea754bf9c6f9fe88cc37e7bdafbe0f
songyoungeun <young5481@gmail.com>
2021-04-27 13:31:47 +0900

    11279_최대힙[우선순위큐]

---
+++ BOJ/자료구조/11279_최대힙.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int n, num;
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n;
+	priority_queue<int>pq;
+	while(n--){
+		cin >> num;
+		if(num == 0){
+			if(!pq.empty()){
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+			else{
+				cout << 0 << '\n';				
+			}
+		}
+		else{
+			pq.push(num);
+		}
+	}
+}
