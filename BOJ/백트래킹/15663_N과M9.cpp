7ad8acb22eea7b710443b79964f2a68e00e747ec
songyoungeun <young5481@gmail.com>
2021-04-29 17:22:37 +0900

    15663_N과M9[백트래킹]

---
+++ BOJ/백트래킹/15663_N과M9.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n,m;
+vector<int>v;
+int arr[8];
+bool isused[8];
+
+void func(int k){ 
+	if(k == m){ 
+	    for(int i = 0; i < m; i++)
+	      cout << arr[i] << ' '; 
+	    cout << '\n';
+	    return;
+  	}
+	int before = 0;
+	
+	for(int i = 0; i < n; i++){ 
+	    if(!isused[i] and before != v[i]){
+	      	before = v[i];
+	      	arr[k] = v[i]; 
+	      	isused[i] = 1; 
+	      	func(k+1);
+	      	isused[i] = 0;
+	    }
+	}
+}
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n >> m;
+  int val;
+  for(int i = 0; i < n; i++){
+  	cin >> val;
+  	v.push_back(val);
+  }
+  sort(v.begin(),v.end());
+  func(0);
+}
