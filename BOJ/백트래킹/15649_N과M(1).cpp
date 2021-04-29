a8d3cbb1e6f47c41acae0ae7b3d40812d10f4660
songyoungeun <young5481@gmail.com>
2021-04-29 14:47:48 +0900

    15649_N과M\(1\)[백트레킹]

--- BOJ/백트래킹/15649_N과M(1).cpp
+++ BOJ/백트래킹/15649_N과M(1).cpp
@@ -10,14 +10,14 @@ void func(int k){ // 현재 k개까지 수를 택했음.
     for(int i = 0; i < m; i++)
       cout << arr[i] << ' '; // arr에 기록해둔 수를 출력
     cout << '\n';
-    cout << "**" << '\n';
+//    cout << "**" << '\n';
     return;
   }
 
   for(int i = 1; i <= n; i++){ // 1부터 n까지의 수에 대해
-    cout << "#####" << '\n';
+//    cout << "#####" << '\n';
     if(!isused[i]){ // 아직 i가 사용되지 않았으면
-      cout<< '@' << k << i << '\n';
+//      cout<< '@' << k << i << '\n';
       arr[k] = i; // k번째 수를 i로 정함
       isused[i] = 1; // i를 사용되었다고 표시
       func(k+1); // 다음 수를 정하러 한 단계 더 들어감
