e850b6bb824cbc44d38d3612488b87e3f0fc4e5d
songyoungeun <young5481@gmail.com>
2021-05-03 22:03:24 +0900

    2458_키순서[플로이드와샬]

--- BOJ/BFS/2458_키순서.cpp
+++ BOJ/BFS/2458_키순서.cpp
@@ -30,22 +30,15 @@ int main(){
 	}
 	floyd();
 	int ans = 0;
-	for(int i = 1; i <= n; i++){
-		for(int j = 1; j <= n; j++){
-			cout << dist[i][j] <<' ';
-		}cout << '\n';
-	}	
+
 	for(int i = 1; i <= n; i++){
 		int cnt = 0;
 		for(int j = 1; j <= n; j++){
 			if(dist[i][j] != INF || dist[j][i] != INF){
-				cout << "1: "<<i << j <<'\n';
-				cout << "2: "<<dist[i][j] <<' '<< dist[j][i] <<'\n';
 				cnt++;
 			}
 		}
 		if(cnt == n-1) ans++;
-		cout << "3: "<<cnt << ans <<'\n';
 	}
 	cout << ans;
 }
