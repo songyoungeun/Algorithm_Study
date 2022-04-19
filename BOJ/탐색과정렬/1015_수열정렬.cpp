944845f1dfcb644d1b2d7806837ea0d099caebe3
songyoungeun <young5481@gmail.com>
2022-04-19 18:13:11 +0900

    1015_수열정렬[정렬]

---
+++ BOJ/탐색과정렬/1015_수열정렬.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+typedef long long int ll;
+using namespace std;
+
+#define NM 55
+
+int N;
+
+struct Elem {
+	int num, idx;
+	bool operator<(const Elem& rhs) const {
+		if (num != rhs.num) return num < rhs.num; //오름차순  
+		return idx < rhs.idx; //오름차순  
+	}
+}B[NM];
+int P[NM];
+
+void input() {
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int x; cin >> x;
+		B[i] = { x, i };
+	}
+}
+
+void pro() {
+	
+	sort(B, B + N);
+	for (int i = 0; i < N; i++) {
+		P[B[i].idx] = i;
+	}
+	for (int i = 0; i < N; i++) {
+		cout << P[i] << " ";
+	}
+}
+
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	input();
+	pro();
+	return 0;
+}
